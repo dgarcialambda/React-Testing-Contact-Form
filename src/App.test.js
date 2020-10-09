6b551a197a81f67c34684070edd79f65750684e2
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-10-08 21:47:51 -0400

    Started My Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("renders form on ContactForm component", async () => {
+  render(<ContactForm />)
+
+  const element = screen.queryByTestId(/lastName/i);
+  expect(element).toBeInTheDocument();
+})
